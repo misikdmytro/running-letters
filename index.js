f06426b6f17506789c97265afc6365c21c4ca9ec
Dmytro Misik <d.misik@draftkings.com>
2023-11-30 13:37:00 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+const main = document.getElementById('main');
+const root = document.getElementById('root');
+const width = document.getElementById('width');
+const cols = document.getElementById('cols');
+
+function generate() {
+    root.innerHTML = '';
+
+    const rowsNum = 25;
+    const colsNum = cols.value;
+
+    for (let row = 0; row < rowsNum; row++) {
+        const parent = document.createElement('div');
+        parent.classList.add('row');
+
+        for (let col = 0; col < colsNum; col++) {
+            const child = document.createElement('div');
+            child.classList.add('col');
+            if (Math.round(colsNum / 2) - 1 === col) {
+                child.style.fontWeight = 'bold';
+            }
+
+            const randomLetter =
+                letters[Math.floor(Math.random() * letters.length)];
+            child.innerText = randomLetter;
+            parent.appendChild(child);
+        }
+
+        root.appendChild(parent);
+    }
+}
+
+function updatePadding() {
+    root.style.padding = `0 ${width.value}px`;
+}
+
+width.addEventListener('input', () => {
+    window.localStorage.setItem('width', width.value);
+    updatePadding();
+});
+
+cols.addEventListener('input', () => {
+    window.localStorage.setItem('cols', cols.value);
+    generate();
+});
+
+width.value = window.localStorage.getItem('width') || main.clientWidth / 10;
+width.max = main.clientWidth;
+width.min = 0;
+
+cols.value = window.localStorage.getItem('cols') || 5;
+
+updatePadding();
+generate();
